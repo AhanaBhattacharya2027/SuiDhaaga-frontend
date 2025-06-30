8f0ad608d5689152b1f19d3e034e10ea6f0dad96
Ahana Bhattacharya <codeart.genius@gmail.com>
2025-06-30 08:38:17 +0530

    linkchange

--- src/Components/TailorDashboard.jsx
+++ src/Components/TailorDashboard.jsx
@@ -11,7 +11,7 @@ const TailorDashboard = () =>{
     const navigate = useNavigate();
 
     useEffect(()=>{
-        fetch(`http://localhost:5000/api/tailor-dashboard/${id}`)
+        fetch(`https://suidhaaga2.onrender.com/api/tailor-dashboard/${id}`)
         .then(res=>res.json())
         .then(data=>{
             if(data.tailor){
